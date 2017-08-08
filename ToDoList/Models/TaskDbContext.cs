8859c93dcca7b866ab394411f20ed39e5625c046
moni20001 <moni20001@abv.bg>
2017-08-08 19:33:24 +0300

    Added the ToDoListLab

---
+++ ToDoList/Models/TaskDbContext.cs
@@ -0,0 +1,17 @@
+namespace ToDoList.Models
+{
+    using System.Data.Entity;
+
+    public class TaskDbContext : DbContext
+    {
+        
+        public TaskDbContext()
+            : base("name=TaskDbContext")
+        {
+        }
+        
+        public virtual DbSet<Task> Tasks { get; set; }
+    }
+
+
+}
